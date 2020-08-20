58a3e9a4e39570b0b6f63b3ccb7b8229d642ae75
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-08-21 01:01:13 +0200

    formulario esperando por token

--- src/Components/FormLeads/FormLeads.jsx
+++ src/Components/FormLeads/FormLeads.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import './FormLeads.scss';
+import { Form, Input, Button, notification } from 'antd';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+}
+
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+}
 
 const FormLeads = () =>  {
+
+    const history = useHistory();
+    const onFinish = user => {
+        axios.post('esperando_token_', user)
+            .then(() => {
+                notification.success({ message: 'Datos enviados' });
+                history.push('/')
+            })
+            .catch(console.error)
+    }
+
+
 
     return (
 
@@ -9,17 +33,45 @@ const FormLeads = () =>  {
             
             <div className="Form FormLeft">
 
+                <img src="/images/gana-ahorro.png" alt=""/>
+
             </div>
 
             <div className="Form FormRight">
 
-                <form className="FormInfo" action="">
+                                <Form
+                        className="FormLogin"
+                        {...layout}
+                        onFinish={onFinish}
+                        onFinishFailed={console.error} >
+                        <Form.Item
+                            label="Nombre"
+                            name="name"
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            label="Email"
+                            name="email"
+                            rules={[{ required: true, message: 'El email es requerido' }]}
+                        >
+                            <Input />
+                        </Form.Item>
 
-                    <label htmlFor="">Nombre</label>
-                    <input type="text"/>
+                        <Form.Item
+                            label="Telefono"
+                            name="telefono"
+                            rules={[{ required: true, message: 'El telefono es requerido' }]}
+                        >
+                            <Input />
+                        </Form.Item>
 
-
-                </form>
+                        <Form.Item {...tailLayout}>
+                            <Button className="ButtonRegister" type="primary" htmlType="submit">
+                                Pronto te llamaremos
+                            </Button>
+                        </Form.Item>
+                    </Form>
 
 
             </div>
